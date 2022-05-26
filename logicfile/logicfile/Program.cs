bf74f2820582aa1337abf006e0155502113313a5
shivani korwan <shivanikorwan63232@gmail.com>
2022-05-26 11:27:36 +0530

    UC1 Check PrimeNumber

--- logicfile/logicfile/Program.cs
+++ logicfile/logicfile/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace Logical_Programs
+{
+    class PrimeNumber
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to check wheather its Prime or not ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                //checking the factors of a number by iterating from 1 to num.
+                if (num % i == 0)
+                    count++;
+            }
+            if (count == 2)
+            {
+                Console.WriteLine(num + " is a Prime number");
+            }
+            else
+            {
+                Console.WriteLine(num + " is not a Prime number");
+            }
+
+        }
+
+    }
+
+}
